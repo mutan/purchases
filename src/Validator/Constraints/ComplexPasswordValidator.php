b8697ed1ed56c9ac235d7100ad047ce5d4fc0096
mutan <akim_now@mail.ru>
2019-04-21 23:31:21 +0300

    Working

--- src/Validator/Constraints/ComplexPasswordValidator.php
+++ src/Validator/Constraints/ComplexPasswordValidator.php
@@ -7,7 +7,6 @@ use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 use Symfony\Component\Validator\Exception\UnexpectedValueException;
 
-
 class ComplexPasswordValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
