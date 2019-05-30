2ebe46e52b8ad69da5c02cd9fbb168fc7910e7ff
mutan <akim_now@mail.ru>
2019-05-31 00:22:02 +0300

    Working

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -6,16 +6,12 @@ use App\Services\LitemfApiService;
 use App\Repository\UserAddressRepository;
 use App\Repository\UserPassportRepository;
 use App\Services\TokenGenerator;
-use Mutan\HelperBundle\Traits\LoggerAwareTrait;
-use Mutan\HelperBundle\Traits\ParameterBagAwareTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController extends AbstractController
 {
-    use LoggerAwareTrait, ParameterBagAwareTrait;
-
     /**
      * @Route("/", name="app_homepage")
      * @param LitemfApiService $litemfApiService
@@ -28,11 +24,6 @@ class WelcomeController extends AbstractController
      */
     public function index(LitemfApiService $litemfApiService, UserAddressRepository $userAddressRepository, UserPassportRepository $userPassportRepository, TokenGenerator $tokenGenerator, $debugLogFile) : Response
     {
-        dump($tokenGenerator->getCustomPassword(10, 0, 0, 10, 0)); die('ok');
-
-
-        $userAddres   = $userAddressRepository->find(2);
-        $userPassport = $userPassportRepository->find(12);
 
         //$litemfApiService->createAddress($userAddres, $userPassport);
 
