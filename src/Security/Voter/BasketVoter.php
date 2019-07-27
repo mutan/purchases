540a55a6cf67dcd3811c6c8812707425f4203956
mutan <akim_now@mail.ru>
2019-07-27 18:20:32 +0300

    Working

--- src/Security/Voter/BasketVoter.php
+++ src/Security/Voter/BasketVoter.php
@@ -5,35 +5,47 @@ namespace App\Security\Voter;
 use App\Entity\Basket;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class BasketVoter extends Voter
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     protected function supports($attribute, $subject)
     {
-        return \in_array($attribute, [
-                'BASKET_OPERATE', //by user
-                'BASKET_MANAGE' //by manager
+        // If the attribute isn't one we support, return false
+        // Only vote on Basket objects inside this voter
+        return in_array($attribute, [
+                'BASKET_EDIT', // by user
             ])
             && $subject instanceof Basket;
     }
 
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
+        // The user must be logged in; if not, deny access
         $user = $token->getUser();
         if (!$user instanceof UserInterface) {
             return false;
         }
 
+        // ROLE_ADMIN can do anything
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
+        // You know $subject is a Basket object, thanks to supports
+        // Check conditions and return true to grant permission
         /** @var Basket $subject */
         switch ($attribute) {
-            case 'BASKET_OPERATE':
+            case 'BASKET_EDIT':
                 if ($subject->getUser() == $user && $subject->isNew()) {
-                    return true;
-                }
-                break;
-            case 'BASKET_MANAGE':
-                if ($subject->getManager() == $user) {
                     return true;
                 }
                 break;
--- src/Security/Voter/ProductVoter.php
+++ src/Security/Voter/ProductVoter.php
@@ -5,35 +5,47 @@ namespace App\Security\Voter;
 use App\Entity\Product;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProductVoter extends Voter
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     protected function supports($attribute, $subject)
     {
-        return \in_array($attribute, [
-                'PRODUCT_OPERATE', //by user
-                'PRODUCT_MANAGE' //by manager
+        // If the attribute isn't one we support, return false
+        // Only vote on Product objects inside this voter
+        return in_array($attribute, [
+                'PRODUCT_EDIT', //by user
             ])
             && $subject instanceof Product;
     }
 
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
+        // The user must be logged in; if not, deny access
         $user = $token->getUser();
         if (!$user instanceof UserInterface) {
             return false;
         }
 
+        // ROLE_ADMIN can do anything
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
+        // You know $subject is a Product object, thanks to supports
+        // Check conditions and return true to grant permission
         /** @var Product $subject */
         switch ($attribute) {
-            case 'PRODUCT_OPERATE':
+            case 'PRODUCT_EDIT':
                 if ($subject->getUser() == $user && $subject->isActive()) {
-                    return true;
-                }
-                break;
-            case 'PRODUCT_MANAGE':
-                if ($subject->getBasket()->getManager() == $user) {
                     return true;
                 }
                 break;
