475b1329d71bad588b052b55e448a1a0e0557d37
mutan <akim_now@mail.ru>
2019-09-02 00:56:43 +0300

    Working

--- src/Services/Entity/OrderHelper.php
+++ src/Services/Entity/OrderHelper.php
@@ -6,27 +6,46 @@ use App\Entity\Order;
 
 class OrderHelper
 {
-    // TODO Ужас ужас переделать
     const STATUSES = [
         Order::STATUS_NEW => [
             'label' => 'Новый',
-            'description' => 'Заказы со статусом Новый можно редактировать и удалять',
+            'description' => 'Заказ и товары в нем можно редактировать и удалять',
             'next_allowed_statuses' => [Order::STATUS_APPROVED, Order::STATUS_DELETED],
         ],
         Order::STATUS_APPROVED => [
             'label' => 'Утвержден',
-            'description' => 'Заказы со статусом Утвержден больше нельзя редактировать или удалять, но можно вернуть обратно в статус Новый.',
+            'description' => 'Заказ и товары в нем больше нельзя редактировать или удалять, но можно вернуть обратно в статус Новый',
             'next_allowed_statuses' => [Order::STATUS_NEW, Order::STATUS_REDEEMED],
         ],
         Order::STATUS_REDEEMED => [
             'label' => 'Выкупается',
-            'description' => 'Менеджер начал выкупать ваш заказ. Редактировать или удалять заказ, а также товары в нем, больше нельзя.',
+            'description' => 'Менеджер начал выкупать ваш заказ. Редактировать или удалять заказ и товары в нем больше нельзя',
             'next_allowed_statuses' => [Order::STATUS_BOUGHT],
         ],
-        Order::STATUS_BOUGHT => 'Выкуплен',
-        Order::STATUS_SENT => 'Отправлен',
-        Order::STATUS_RECEIVED => 'Получен',
-        Order::STATUS_CANCELED => 'Отменен',
-        Order::STATUS_DELETED => 'Удален',
+        Order::STATUS_BOUGHT => [
+            'label' => 'Выкуплен',
+            'description' => 'Заказ выкуплен менеджером. Это самый долгий статус',
+            'next_allowed_statuses' => [Order::STATUS_SENT],
+        ],
+        Order::STATUS_SENT => [
+            'label' => 'Отправлен',
+            'description' => 'Заказ отправлен вам',
+            'next_allowed_statuses' => [Order::STATUS_RECEIVED],
+        ],
+        Order::STATUS_RECEIVED => [
+            'label' => 'Получен',
+            'description' => 'Вы получили заказ',
+            'next_allowed_statuses' => [],
+        ],
+        Order::STATUS_CANCELED => [
+            'label' => 'Отменен',
+            'description' => 'Заказ отменен клиентом, виден клиенту',
+            'next_allowed_statuses' => [],
+        ],
+        Order::STATUS_DELETED => [
+            'label' => 'Удален',
+            'description' => 'Заказ удален по инициативе клиента, не виден клиенту',
+            'next_allowed_statuses' => [],
+        ],
     ];
 }
